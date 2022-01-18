30e0844b53d69431928f3f78fb03f2fa6c051b3f
rajarai37 <rajarai37@gmail.com>
2022-01-18 15:23:53 +0530

    CanDrive Submission

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,19 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if (hasDrivingLiscence==true){
+        if (isTired==true){
+            return ("You shouldn't drive")
+        }
+        else if(isSober==false){
+            return ("You shouldn't drive")
+        }
+        else{
+            return ("You can drive")
+        }
+    }
+    else{
+        return("You cannot drive")
+    }
 }
 
 module.exports = CanDrive;
